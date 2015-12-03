22eb4babf7ba58a3bb9867817e000b6550741728
Ivan Rocha <irocha@tripadvisor.com>
2015-12-03 16:32:12 -0500

    V1 - Working

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # watermark
 Python script to add a watermark or logo to images
+
+### Requirements
+Pillow:
+```
+pip install pillow
+```
+
+[Pillow Docs](https://python-pillow.github.io/)
+
+### Usage
+This script allows you to add a watermark or logo to images in a specified folder. The script takes three arguments:
+
+1. The folder with the images you want to watermark
+2. The path of the logo to add
+3. The position you want to place the logo (optional)
+
+These are the valid positions:
+
+- topleft
+- topright
+- bottomleft
+- bottomright
+- center (if no position is specified, this will be the default)
+
+Any other position will result in an error.
+
+To use watermark.py without specifying a position:
+
+
+```
+python watermark.py  './images' 'logo.png'
+```
+
+To use watermark.py and specify a position:
+
+```
+python watermark.py  './images' 'logo.png' bottomright
+```
+
+### Adapting
+If you want to save your watermarked images as new files instead of saving over the existing files, simply add a prefix to any image.save() lines:
+
+```
+image.save(path + '/fancy_new_prefix_' + filename)
+```
---
+++ watermark.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from PIL import Image
+
+
+if len(sys.argv) < 3:
+    print('Usage: watermark.py \'image folder path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
+    sys.exit()
+elif len(sys.argv) == 4:
+    path = sys.argv[1]
+    lgo = sys.argv[2]
+    pos = sys.argv[3]
+else:
+    path = sys.argv[1]
+    lgo = sys.argv[2]
+
+
+logo = Image.open(lgo)
+logoWidth = logo.width
+logoHeight = logo.height
+
+
+for filename in os.listdir(path):
+    if (filename.endswith('.jpg') or filename.endswith('.png')) and (filename != lgo):
+
+        image = Image.open(path + '/' + filename)
+        imageWidth = image.width
+        imageHeight = image.height
+
+        try:
+            if pos == 'topleft':
+                image.paste(logo, (0, 0), logo)
+            elif pos == 'topright':
+                image.paste(logo, (imageWidth - logoWidth, 0), logo)
+            elif pos == 'bottomleft':
+                image.paste(logo, (0, imageHeight - logoHeight), logo)
+            elif pos == 'bottomright':
+                image.paste(logo, (imageWidth - logoWidth, imageHeight - logoHeight), logo)
+            elif pos == 'center':
+                image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
+            else:
+                print('Error: ' + pos + ' is not a valid position')
+                print('Usage: watermark.py \'image path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
+
+            image.save(path + '/' + filename)
+            print('Added watermark to ' + path + '/' + filename)
+
+        except:
+            image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
+            image.save(path + '/' + filename)
+            print('Added default watermark to ' + path + '/' + filename)
