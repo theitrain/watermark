4fbfc5f35e80ca9c8ea99dd16c19c966c9e43c36
oshoura <omarshoura1@gmail.com>
2023-10-02 18:55:21 -0400

    V2: More customizing features Description: This PR introduces a series of enhancements and improvements to the original watermarking script, aiming to make it more user-friendly, versatile, and robust.
    Major Changes:
    CLI Arguments with argparse:
    
    Moved away from sys.argv to the more flexible argparse.
    Arguments are clearly defined with help messages.
    Introduced optional arguments for better control.
    Support for Subfolders:
    
    Implemented recursive search using os.walk() to process images in subfolders.
    Dynamic Watermark Scaling:
    
    The watermark can now be resized based on the dimensions of the target image or a user-specified scale.
    Save to New Directory Option:
    
    Users can specify a new directory to save watermarked images, preserving the original directory structure.
    Watermark Padding:
    
    Introduced an option to add padding around the watermark, especially useful when it's placed in corners.
    Enhanced Error Handling:
    
    Incorporated better error handling mechanisms using try-except blocks to manage potential issues such as invalid image formats.
    File Extension Check:
    
    Enhanced the file extension check to include .jpeg.
    Removed:
    Deprecated and unsafe exception handling that could result in unintentional overwriting of images.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# watermark
-Python script to add a watermark or logo to images
+# Watermark
+Python script to add a watermark or logo to images recursively in a specified directory and its subdirectories.
 
 ### Requirements
 Pillow:
@@ -10,11 +10,13 @@ pip install pillow
 [Pillow Docs](https://python-pillow.github.io/)
 
 ### Usage
-This script allows you to add a watermark or logo to images in a specified folder. The script takes three arguments:
+This script allows you to add a watermark or logo to images in a specified folder and its subdirectories. The script takes several arguments:
 
 1. The folder with the images you want to watermark
 2. The path of the logo to add
 3. The position you want to place the logo (optional)
+4. The directory where you want to save the watermarked images (optional; if not provided, the watermarked images will overwrite the original images)
+5. Padding (in pixels) around the watermark logo (optional; default is 0)
 
 These are the valid positions:
 
@@ -26,26 +28,28 @@ These are the valid positions:
 
 Any other position will result in an error.
 
-To use watermark.py without specifying a position:
-
-
-```
-python watermark.py  './images' 'logo.png'
-```
-
-To use watermark.py and specify a position:
+To use watermark.py, specify a position, a destination directory, and scale the watermark to 30% of the image width:
 
 ```
-python watermark.py  './images' 'logo.png' bottomright
+python watermark.py './images' 'logo.png' --pos bottomright --new_dir './watermarked_images' --scale 30
+```
+
+
+To use watermark.py, specify a position and a destination directory:
+
+```
+python watermark.py './images' 'logo.png' --pos bottomright --new_dir './watermarked_images'
+```
+
+To add padding around the watermark:
+
+```
+python watermark.py './images' 'logo.png' --pos topleft --padding 20
 ```
 
 ### Adapting
-If you want to save your watermarked images as new files instead of saving over the existing files, simply add a prefix to any image.save() lines:
-
-```
-image.save(path + '/fancy_new_prefix_' + filename)
-```
+If you want to save your watermarked images to a different directory instead of saving over the existing files, use the `--new_dir` option as shown in the usage examples. The script will maintain the original folder hierarchy in the specified new directory.
 
 ### Ideas for future improvements
-- allow custom positioning
-- adjust watermarks to be semi-transparent
+- Allow custom positioning via exact pixel coordinates.
+- Adjust watermarks to be semi-transparent.
--- watermark.py
+++ watermark.py
@@ -1,51 +1,132 @@
 import os
-import sys
+import argparse
+from PIL import Image, UnidentifiedImageError
 
-from PIL import Image
+def add_watermark(directory, logo_path, position, new_directory, padding, scale):
+    """
+    Add a watermark to images in the specified directory.
+    
+    Args:
+    - directory (str): The directory containing images to be watermarked.
+    - logo_path (str): Path to the watermark logo.
+    - position (str): Position of the watermark on the image.
+    - new_directory (str): Directory to save watermarked images.
+    - padding (int): Padding around the logo in pixels.
+    """
+    EXTS = ('.jpg', '.jpeg', '.png')
 
-EXTS = ('.jpg', '.png')
+    try:
+        original_logo = Image.open(logo_path)
+    except UnidentifiedImageError:
+        print(f"Failed to read logo from {logo_path}. Ensure it's a valid image format.")
+        return
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
 
-if len(sys.argv) < 3:
-    print('Usage: watermark.py \'image folder path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
-    sys.exit()
-elif len(sys.argv) == 4:
-    path = sys.argv[1]
-    lgo = sys.argv[2]
-    pos = sys.argv[3]
-else:
-    path = sys.argv[1]
-    lgo = sys.argv[2]
+    # Check if the logo has an alpha channel
+    if original_logo.mode == 'RGBA':
+        logo_mask_original = original_logo.split()[3]
+    else:
+        logo_mask_original = None
 
-logo = Image.open(lgo)
-logoWidth = logo.width
-logoHeight = logo.height
+    # Using os.walk() to make folder image search recursive
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.lower().endswith(EXTS) and filename != os.path.basename(logo_path):
+                full_path = os.path.join(dirpath, filename)
+                try:
+                    image = Image.open(full_path)
+                except UnidentifiedImageError:
+                    print(f"Skipped {filename}. Unsupported image format.")
+                    continue
+                except Exception as e:
+                    print(f"An error occurred while processing {filename}: {e}")
+                    continue
+                
+                full_path = os.path.join(dirpath, filename)
+                image = Image.open(full_path)
+                imageWidth, imageHeight = image.size
+
+                
+                shorter_side = min(imageWidth, imageHeight)
+                new_logo_width = int(shorter_side * scale/100)
+                logo_aspect_ratio = original_logo.width / original_logo.height
+                new_logo_height = int(new_logo_width / logo_aspect_ratio)
+
+                # Resize the logo and its mask
+                logo = original_logo.resize((new_logo_width, new_logo_height))
+                if logo_mask_original:
+                    logo_mask = logo_mask_original.resize((new_logo_width, new_logo_height))
+                else:
+                    logo_mask = None
+
+                paste_x, paste_y = 0, 0
+
+                if position == 'topleft':
+                    paste_x, paste_y = padding, padding
+                elif position == 'topright':
+                    paste_x, paste_y = imageWidth - new_logo_width - padding, padding
+                elif position == 'bottomleft':
+                    paste_x, paste_y = padding, imageHeight - new_logo_height - padding
+                elif position == 'bottomright':
+                    paste_x, paste_y = imageWidth - new_logo_width - padding, imageHeight - new_logo_height - padding
+                elif position == 'center':
+                    paste_x, paste_y = (imageWidth - new_logo_width) // 2, (imageHeight - new_logo_height) // 2
+
+                try:
+                    image.paste(logo, (paste_x, paste_y), logo_mask)
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+
+                # Determine the relative path
+                relative_path = os.path.relpath(dirpath, directory)
+                save_directory = new_directory if new_directory else directory
+                final_save_directory = os.path.join(save_directory, relative_path)
+
+                # Ensure the new directory exists
+                if not os.path.exists(final_save_directory):
+                    os.makedirs(final_save_directory)
+
+                new_image_path = os.path.join(final_save_directory, filename)
+                image.save(new_image_path)
+                print('Added watermark to ' + new_image_path)
+                
+    original_logo.close()
 
 
-for filename in os.listdir(path):
-    if any([filename.lower().endswith(ext) for ext in EXTS]) and filename != lgo:
-        image = Image.open(path + '/' + filename)
-        imageWidth = image.width
-        imageHeight = image.height
 
-        try:
-            if pos == 'topleft':
-                image.paste(logo, (0, 0), logo)
-            elif pos == 'topright':
-                image.paste(logo, (imageWidth - logoWidth, 0), logo)
-            elif pos == 'bottomleft':
-                image.paste(logo, (0, imageHeight - logoHeight), logo)
-            elif pos == 'bottomright':
-                image.paste(logo, (imageWidth - logoWidth, imageHeight - logoHeight), logo)
-            elif pos == 'center':
-                image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
-            else:
-                print('Error: ' + pos + ' is not a valid position')
-                print('Usage: watermark.py \'image path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A script to add watermarks to images. Given a directory, this will traverse through all its images and apply the specified watermark. The resulting watermarked images can be saved in the same directory or a new specified directory, maintaining the original directory structure.")
 
-            image.save(path + '/' + filename)
-            print('Added watermark to ' + path + '/' + filename)
+    parser.add_argument('dir', 
+                        help="Directory containing the images you want to watermark. The script will search recursively within this directory.",
+                        metavar='SourceDirectory')
 
-        except:
-            image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
-            image.save(path + '/' + filename)
-            print('Added default watermark to ' + path + '/' + filename)
+    parser.add_argument('logo', 
+                        help="Path to the logo image that will be used as the watermark.",
+                        metavar='WatermarkLogoPath')
+
+    parser.add_argument('--pos', 
+                        choices=['topleft', 'topright', 'bottomleft', 'bottomright', 'center'], 
+                        default='center',
+                        help="Specifies the position of the watermark on the image. Default is 'center'.")
+
+    parser.add_argument('--new_dir',
+                        default=None, 
+                        help="An optional directory where the watermarked images will be saved. If not provided, watermarked images will overwrite originals in the source directory. The original directory structure will be maintained.",
+                        metavar='DestinationDirectory')
+
+    parser.add_argument('--padding', 
+                        type=int, 
+                        default=0,
+                        help="Specifies the padding (in pixels) around the watermark, useful when watermark is positioned at the corners. Default is 0, meaning no padding.")
+    parser.add_argument('--scale',
+                        type=float, 
+                        default=20,
+                        help="Resize the watermark based on a percentage of the image's width. E.g., for 10% of the image's width, provide 10.")
+
+
+    args = parser.parse_args()
+
+    add_watermark(args.dir, args.logo, args.pos, args.new_dir, args.padding)
